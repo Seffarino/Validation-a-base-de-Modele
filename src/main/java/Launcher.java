a24871059f5d487bdc61b78f4237824fd86163a3
Amayas <aseffar>
2025-12-01 11:46:41 +0100

    Dependancies added

---
+++ src/main/java/Launcher.java
@@ -0,0 +1,22 @@
+import nz.ac.waikato.modeljunit.FsmModel;
+import nz.ac.waikato.modeljunit.RandomTester;
+import nz.ac.waikato.modeljunit.Tester;
+import nz.ac.waikato.modeljunit.VerboseListener;
+
+import java.io.File;
+import java.io.FileNotFoundException;
+import java.io.IOException;
+
+public class Launcher {
+    public static void main(String[] args) throws IOException {
+        FsmModel model = new SCANETTE1();
+        Tester tester = new RandomTester(model);
+        JUnitExporter testMaker = new JUnitExporter();
+        tester.addListener(testMaker);
+        tester.buildGraph().printGraphDot("./graphDAB1.dot");
+        tester.generate(20);
+        File file = new File("./tests.java");
+        testMaker.exportTest("test",file);
+
+    }
+}
